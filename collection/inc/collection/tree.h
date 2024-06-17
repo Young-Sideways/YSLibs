82a325c6f5ac43b708e19473f6e7e6598f3dc70f
Dmitrii Basyrov <d.basyrov@baum.ru>
2024-06-17 05:52:47 +0300

    Initial tree commit

---
+++ collection/inc/collection/tree.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * @file      tree.h
+ * @author    Young Sideways
+ * @brief     tree collection
+ * @date      17.06.2024
+ * @copyright young.sideways@mail.ru, Copyright (c) 2024. All right reserved.
+ ******************************************************************************/
+
+#ifndef TREE_H_
+#define TREE_H_
+
+#pragma once
+
+#pragma region --- INCLUDE ---
+
+#pragma endregion
+
+#pragma region --- MACRO ---
+
+#pragma endregion
+
+#pragma region --- TYPEDEF ---
+
+#pragma endregion
+
+#pragma region --- CONSTRUCTOR/DESTRUCTOR ---
+
+#pragma endregion
+
+#pragma region --- FUNCTION ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#endif // !TREE_H_
---
+++ collection/src/tree.c
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * @file      tree.h
+ * @author    Young Sideways
+ * @brief     tree collection
+ * @date      17.06.2024
+ * @copyright young.sideways@mail.ru, Copyright (c) 2024. All right reserved.
+ ******************************************************************************/
+
+#include "collection/tree.h"
+
+#pragma region --- INCLUDE ---
+
+#pragma endregion
+
+#pragma region --- MACRO ---
+
+#pragma endregion
+
+#pragma region --- TYPEDEF ---
+
+#pragma endregion
+
+#pragma region --- CONSTRUCTOR/DESTRUCTOR ---
+
+#pragma endregion
+
+#pragma region --- FUNCTION ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
