e3c46c0e85da3aa6899130a77f0237825175ffcb
Young-Sideways <young.sideways@mail.ru>
2024-06-19 02:22:29 +0300

    changes to task1
    Signed-off-by: Dmitrii Basyrov <d.basyrov@baum.ru>

--- task1/task1.c
+++ task1/task1.c
@@ -1,9 +1,9 @@
 /*******************************************************************************
  *  @file      task1.c
- *  @brief     Ответы на 1 задание
+ *  @brief     РџРѕРґРїСЂРѕРіСЂР°РјРјР° РІС‹РїРѕР»РЅРµРЅРёСЏ РґР»СЏ С‚РµРјС‹ 1
  *  @author    Young Sideways
  *  @date      19.02.2024
- *  @copyright © young.sideways@mail.ru, 2024. All right reserved.
+ *  @copyright young.sideways@mail.ru, Copyright (c) 2024. All right reserved.
  ******************************************************************************/
 
 #include "task1.h"
