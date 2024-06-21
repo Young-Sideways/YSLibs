8dee63c29b9c1b653ca9fa6d56da35a191ff801b
Dmitrii Basyrov <d.basyrov@baum.ru>
2024-06-21 10:59:14 +0300

    Main changes to macro and algorithm sublib

---
+++ collection/inc/collection/btree.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * @file      btree.h
+ * @author    Young Sideways
+ * @brief     tree collection
+ * @date      19.06.2024
+ * @copyright young.sideways@mail.ru, Copyright (c) 2024. All right reserved.
+ ******************************************************************************/
+
+#ifndef BTREE_H_
+#define BTREE_H_
+
+#pragma once
+
+#pragma region --- INCLUDE ---
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "collection/general.h"
+
+#pragma endregion
+
+#pragma region --- MACRO ---
+
+#define BTREE_SIZE_MIN COLLECTION_SIZE_MIN
+#define BTREE_SIZE_MAX COLLECTION_SIZE_MAX
+
+#pragma endregion
+
+#pragma region --- TYPEDEF ---
+
+#pragma endregion
+
+#pragma region --- CONSTRUCTOR/DESTRUCTOR ---
+
+#pragma endregion
+
+#pragma region --- FUNCTION ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#pragma region ---  ---
+
+#pragma endregion
+
+#endif // !BTREE_H_
---
+++ collection/inc/collection/tuple.h
@@ -0,0 +1,8 @@
+//
+// Created by pc on 21.06.24.
+//
+
+#ifndef YSLIBS_TUPLE_H
+#define YSLIBS_TUPLE_H
+
+#endif //YSLIBS_TUPLE_H
