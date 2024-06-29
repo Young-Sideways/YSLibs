2635c41f39935b22ba76cf9c7c6529ead26b15b1
Young-Sideways <young.sideways@mail.ru>
2024-06-30 01:21:33 +0300

    cleaning repo files

--- core/debug.h
+++ core/debug.h
@@ -3,7 +3,7 @@
  *  @brief     debug module helps debug your code
  *  @author    Young Sideways
  *  @date      18.04.2024
- *  @copyright © Young Sideways, 2024. All right reserved.
+ *  @copyright young.sideways@mail.ru, Copyright (c) 2024. All right reserved.
  ******************************************************************************/
 
 #ifndef DEBUG_H_
@@ -25,7 +25,7 @@
 #else
     #define explain_assert(expression, msg) assert((expression) && msg)
     #define explain_error(msg) explain_assert(0, msg)
-#endif
+#endif 
 
 #pragma endregion
 
